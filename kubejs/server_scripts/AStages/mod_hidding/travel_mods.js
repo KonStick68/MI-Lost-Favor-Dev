ff3edaec176227053d7d461986bdc790cae53f54
Kisuny <homeca19@gmail.com>
2025-08-12 23:26:24 +0300

    comment for mod  restrictions for now

--- kubejs/server_scripts/AStages/mod_hidding/travel_mods.js
+++ kubejs/server_scripts/AStages/mod_hidding/travel_mods.js
@@ -1,18 +1,18 @@
-const travel_mods = [
-    "apothic_compats",
-    "artifacts",
-    "cataclysm",
-    "relics",
-    "relicsofrain",
-    "reliquified_lenders_cataclysm",
-    "arcane_abilities",
-    "fdbosses",
-    "ribbits",
-]
+// const travel_mods = [
+//     "apothic_compats",
+//     "artifacts",
+//     "cataclysm",
+//     "relics",
+//     "relicsofrain",
+//     "reliquified_lenders_cataclysm",
+//     "arcane_abilities",
+//     "fdbosses",
+//     "ribbits",
+// ]
 
-travel_mods.forEach(mod => {
-    AStages.addRestrictionForMod(`astages/mod/${mod}`, mod, mod)
-    .setCanPickedUp(true)
-    .setRenderItemName(true)
-});
+// travel_mods.forEach(mod => {
+//     AStages.addRestrictionForMod(`astages/mod/${mod}`, mod, mod)
+//     .setCanPickedUp(true)
+//     .setRenderItemName(true)
+// });
     
--- kubejs/server_scripts/AStages/mod_hidding/util_mods.js
+++ kubejs/server_scripts/AStages/mod_hidding/util_mods.js
@@ -1,27 +1,27 @@
-const util_mods = [
-    "simplemagnets",
-    "buildinggadgets2",
-    "magnumtorch",
-    "apotheosis",
-    "tesseract",
-    "travelanchors",
-    "sophisticatedstorage",
-    "waystones",
-    "apothic_enchanting",
-    "sophisticatedbackpacks",
-    "javd",
-    "naturescompass",
-    "measurements",
-    "adfinders",
-    "tempad",
-    "dankstorage",
-    "gag",
-    "elevatorid",
-    "hangglider",
-]
+// const util_mods = [
+//     "simplemagnets",
+//     "buildinggadgets2",
+//     "magnumtorch",
+//     "apotheosis",
+//     "tesseract",
+//     "travelanchors",
+//     "sophisticatedstorage",
+//     "waystones",
+//     "apothic_enchanting",
+//     "sophisticatedbackpacks",
+//     "javd",
+//     "naturescompass",
+//     "measurements",
+//     "adfinders",
+//     "tempad",
+//     "dankstorage",
+//     "gag",
+//     "elevatorid",
+//     "hangglider",
+// ]
 
-util_mods.forEach(mod => {
-    AStages.addRestrictionForMod(`astages/mod/${mod}`, mod, mod)
-    .setCanPickedUp(true)
-});
+// util_mods.forEach(mod => {
+//     AStages.addRestrictionForMod(`astages/mod/${mod}`, mod, mod)
+//     .setCanPickedUp(true)
+// });
     
